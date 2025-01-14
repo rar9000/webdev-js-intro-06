4ad94dc4506e1c9b3a4963f2519ac7631cf03c9f
rar9000 <clavius@Clavius.local>
2025-01-14 14:50:09 -0500

    completed

--- js/assignment.js
+++ js/assignment.js
@@ -9,29 +9,43 @@ function evenOrOdd() {
     const num = 3;
     // Write the logic to decide if the variable "num" is even or odd
     // and set the element's value the string "Even" or "Odd" accordingly
-
+    for (let isChecked = false; isChecked === false; isChecked = true) {
+        if (num % 2 === 0) {
+            evenOrOddElement.innerText = "Even";
+        } 
+        else {
+            evenOrOddElement.innerText = "Odd";
+        }
+    }
 }
+   
 
 function sumTheNumbers() {
     let sum = 0;
     // Write the logic to sum the numbers 1 through 10
     // using a for loop. Check the expected output
     // on the assignment page
-
+    for (let i = 1; i <= 10; i++) {
+        sum += i; 
+        sumTheNumbersElement.innerText  = sum;
+    }
 }
 
 function createNumberArray() {
     const numberArray = [];
-
     // Write the logic that loops 10 times and adds the value
     // to numberArray each iteration. Check the expected output
     // on the assignment page
-
+    for (let i = 1; i <= 10; i++) {
+        numberArray.push(i); 
+        createNumberArrayElement.innerText  = numberArray;
+    }
 }
 
 function render() {
-    // Call the created functions
-
+    evenOrOdd();
+    sumTheNumbers();
+    createNumberArray()
 }
 
 submissionBtn.addEventListener("click", function () {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,50 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 100; i++) {
+    if (i === 23) {
+        console.log("Breaking the loop at:", i);
+        break;
+    }
+    console.log(i);
+}
+
+
+let i = 0;
+while (i < 356) {
+    console.log(i);
+    i++;
+}    
+
+let count = 0;
+while (count < 356) {
+    if (count === 3) {
+        break; 
+    }
+    console.log(count);
+    count++;
+}
+
+let i = 0;
+do {
+    console.log(i);
+    i--;
+} while (i > -2345);
+
+let num = 0;
+do {
+    num--;
+    if (num === -9) {
+        continue; 
+    }
+    console.log(num);
+} while (num > -2345);
+
+*/
